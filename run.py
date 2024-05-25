21be527a884f1eafb0171e78fe4f8073ae1d3849
Abdur Rahim Bhuiyan <zamshedbhai@yahoo.com>
2024-05-25 22:20:58 +0000

    Create get_mark_data function to get data string from the user

--- run.py
+++ run.py
@@ -14,8 +14,17 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('mark_sheet')
 
-mark = SHEET.worksheet('mark')
 
-data = mark.get_all_values()
+def get_mark_data():
+    """
+    Get Mark figures input from the user.
+    """
+    print("Please enter Mark data.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 90,80,95,80,70,30\n")
 
-print(data)
+    data_str = input("Enter your Mark here: ")
+    print(f"The data provided is {data_str}")
+
+
+get_mark_data()
